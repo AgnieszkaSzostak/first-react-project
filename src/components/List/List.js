fa8a4ee97c98a68053e6e153e2bb402b37068884
Agnieszka Szostak <aa.szostak3@student.uw.edu.pl>
2021-11-11 14:45:19 +0100

    Add Hero.js and List.js components

---
+++ src/components/List/List.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styles from './List.scss';
+import Hero from '../Hero/Hero.js';
+// import ;
+
+class List extends React.Component {
+  render() {
+    return (
+      <section className= {styles.component}>
+          <Hero />
+      </section>
+    )
+  }
+}
+
+export default List;
