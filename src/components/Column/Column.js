b8698d56801b8056c9092162c64969dd0078adef
Agnieszka Szostak <aa.szostak3@student.uw.edu.pl>
2021-11-12 18:38:36 +0100

    Change List.js & Column.js & Add Creator-Button-Icon component

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -4,13 +4,11 @@ import PropTypes from 'prop-types';
 
 
 class Column extends React.Component {
-  static propTypes = {
-    columnTitle: PropTypes.node.isRequired
-  }
+  
   render() {
     return (
       <section className={styles.component}>
-          <h3 className={styles.title} >{this.props.columnTitle}</h3>
+          <h3 className={styles.title} >{this.props.title}</h3>
       </section>
     )
   }
--- src/components/List/List.js
+++ src/components/List/List.js
@@ -3,28 +3,31 @@ import styles from './List.scss';
 import Hero from '../Hero/Hero.js';
 import PropTypes from 'prop-types';
 import Column from '../Column/Column.js';
-
+import {settings} from '../../data/dataStore.js';
 
 class List extends React.Component {
+  state = {
+    columns: this.props.columns || [],
+  }
   static propTypes = {
     title: PropTypes.node.isRequired,
-    children: PropTypes.node,
-    link: PropTypes.string.isRequired
+    description: PropTypes.node,
+    columns: PropTypes.array,
   }
   static defaultProps = {
-    children: <p>I can do all the things!!!</p>,
+    description: settings.defaultListDescription,
   }
   render() {
     return (
       <section className={styles.component}>
-        <Hero titleText={this.props.title} link={this.props.link}/>
+        <Hero titleText={this.props.title} link={this.props.image}/>
         <div className={styles.description}>
-          {this.props.children}
+          {this.props.description}
         </div>
         <div className={styles.columns}>
-            <Column columnTitle={'Work'}/>
-            <Column columnTitle={'Home'}/>
-            <Column columnTitle={'Hobby'}/>
+          {this.state.columns.map(({key, ...columnProps}) => (
+            <Column key={key} {...columnProps} />
+          ))}
         </div>
       </section>
     )
