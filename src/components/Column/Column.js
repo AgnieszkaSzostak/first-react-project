16f543f3c449fb6a5b5e89ed314aa65d94681db5
Agnieszka Szostak <aa.szostak3@student.uw.edu.pl>
2021-11-12 21:49:11 +0100

    Add icons to Columns.js

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,17 +1,37 @@
 import React from 'react';
 import styles from './Column.scss';
 import PropTypes from 'prop-types';
+// import {settings} from '../../data/dataStore.js';
+import Card from '../Card/Card.js';
+import Icon from '../Icon/Icon.js';
 
 
 class Column extends React.Component {
-  
+  // state = {
+  //   cards: this.props.columns.cards || [],
+  // }
+  // static propTypes = {
+  //   cards: PropTypes.array,
+    
+  // }
   render() {
     return (
       <section className={styles.component}>
-          <h3 className={styles.title} >{this.props.title}</h3>
+          <h3 className={styles.title} >{this.props.title}
+            <span className={styles.icon}>
+              <Icon name={this.props.icon}></Icon>
+            </span>
+          </h3>
+          {/* <div>
+            {this.state.cards.maps(({key, ...cardProps}) => (
+              <Card key={key} {...cardProps} />
+            ))}
+          </div> */}
+      
+            
       </section>
     )
   }
 }
-
+// console.log('state.cards', state.cards);
 export default Column;
--- src/components/List/List.js
+++ src/components/List/List.js
@@ -18,6 +18,21 @@ class List extends React.Component {
   static defaultProps = {
     description: settings.defaultListDescription,
   }
+  addColumn(title){
+    this.setState(state => (
+      {
+        columns: [
+          ...state.columns,
+          {
+            key: state.columns.length ? state.columns[state.columns.length-1].key+1 : 0,
+            title,
+            icon: 'list-alt',
+            cards: []
+          }
+        ]
+      }
+    ));
+  }
   render() {
     return (
       <section className={styles.component}>
@@ -36,21 +51,7 @@ class List extends React.Component {
       </section>
     )
   }
-  addColumn(title){
-    this.setState(state => (
-      {
-        columns: [
-          ...state.columns,
-          {
-            key: state.columns.length ? state.columns[state.columns.length-1].key+1 : 0,
-            title,
-            icon: 'list-alt',
-            cards: []
-          }
-        ]
-      }
-    ));
-  }
+ 
 }
-console.log('this', this);
+
 export default List;
