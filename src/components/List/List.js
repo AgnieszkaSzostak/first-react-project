b14c6b9fa7929fa8568551f540a7e34008af436b
Agnieszka Szostak <aa.szostak3@student.uw.edu.pl>
2021-11-12 18:51:56 +0100

    Add 'addColumn' method in List.js

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -4,6 +4,7 @@ import Hero from '../Hero/Hero.js';
 import PropTypes from 'prop-types';
 import Column from '../Column/Column.js';
 import {settings} from '../../data/dataStore.js';
+import Creator from '../Creator/Creator.js';
 
 class List extends React.Component {
   state = {
@@ -29,9 +30,27 @@ class List extends React.Component {
             <Column key={key} {...columnProps} />
           ))}
         </div>
+        <div className={styles.creator}>
+          <Creator text={settings.columnCreatorText} action={title => this.addColumn(title)}/>
+        </div>
       </section>
     )
   }
+  addColumn(title){
+    this.setState(state => (
+      {
+        columns: [
+          ...state.columns,
+          {
+            key: state.columns.length ? state.columns[state.columns.length-1].key+1 : 0,
+            title,
+            icon: 'list-alt',
+            cards: []
+          }
+        ]
+      }
+    ));
+  }
 }
-
+console.log('this', this);
 export default List;
