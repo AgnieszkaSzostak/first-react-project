7057e8491db93957416fdca94f43340a4b4e749c
Agnieszka Szostak <aa.szostak3@student.uw.edu.pl>
2021-11-13 13:57:41 +0100

    Add Card components to Column.js

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import styles from './Column.scss';
 import PropTypes from 'prop-types';
-
 import Card from '../Card/Card.js';
 import Icon from '../Icon/Icon.js';
 import Creator from '../Creator/Creator.js';
@@ -37,14 +36,14 @@ class Column extends React.Component {
               <Icon name={this.props.icon}></Icon>
             </span>
           </h3>
-          {/* <div className={styles.cards}>
+          <div>
             {this.state.cards.maps(({key, ...cardProps}) => (
               <Card key={key} {...cardProps} />
             ))}
-          </div> */}
-        <div className={styles.creator}>
-          <Creator text={settings.cardCreatorText} action={title => this.addCard(title)}/>
-        </div>
+          </div>
+          <div className={styles.creator}>
+            <Creator text={settings.cardCreatorText} action={title => this.addCard(title)}/>
+          </div>
         
       </section>
     )
