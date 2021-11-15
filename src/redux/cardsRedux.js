3f7b4d22bb8702e9375107b13280e9141825409b
Agnieszka Szostak <aa.szostak3@student.uw.edu.pl>
2021-11-15 20:55:19 +0100

    Change searchStringRedux.js

--- src/redux/cardsRedux.js
+++ src/redux/cardsRedux.js
@@ -1,7 +1,10 @@
 import shortid from 'shortid';
 
 // selectors
-export const getCardsForColumn = ({cards}, columnId) => cards.filter(card => card.columnId == columnId);
+export const getCardsForColumn = ({cards}, columnId, searchString) => cards.filter(card => card.columnId == columnId
+  && 
+  card.title  == new RegExp(searchString, 'i').test(card.title),
+);
 
 // action name creator
 const reducerName = 'cards';
--- src/redux/searchStringRedux.js
+++ src/redux/searchStringRedux.js
@@ -5,16 +5,18 @@ export const countAllCards = ({cards}) => cards.length;
 
 export const countVisibleCards = ({cards, searchString}) => cards.filter(card => new RegExp(searchString, 'i').test(card.title)).length;
 // action name creator
-// const reducerName = 'search';
+const reducerName = 'search';
+const createActionName = name => `app/${reducerName}/${name}`;
 // actions types
-
+export const CHANGE = createActionName('CHANGE');
 // action creators
-
+export const changeSearchString = payload => ({ payload: { ...payload }, type: CHANGE });
 // reducer
 export default function reducer(statePart = '', action = {}) {
   switch (action.type) {
+    case CHANGE:
+      return [...statePart, action.payload];
     default:
       return statePart;
   }
 }
-
