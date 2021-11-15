a17c6dd5b8122827c6c4074ce16de4fa2c210922
Agnieszka Szostak <aa.szostak3@student.uw.edu.pl>
2021-11-15 18:18:51 +0100

    Change SearchContainer.js

--- src/components/Search/SearchContainer.js
+++ src/components/Search/SearchContainer.js
@@ -4,7 +4,7 @@ import {
   getSearchString,
   countVisibleCards,
   countAllCards,
-  createAction_changeSearchString,
+  changeSearchString,
 } from '../../redux/searchStringRedux';
 
 const mapStateToProps = (state) => ({
@@ -14,7 +14,7 @@ const mapStateToProps = (state) => ({
 });
 
 const mapDispatchToProps = (dispatch) => ({
-  changeSearchString: newSearchString => dispatch(createAction_changeSearchString(newSearchString)),
+  changeSearchString: newSearchString => dispatch(changeSearchString(newSearchString)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Search);
