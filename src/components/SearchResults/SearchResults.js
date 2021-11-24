295dd7d4757f6d6025a62f76b8b9e4e92a0124ba
Agnieszka Szostak <aa.szostak3@student.uw.edu.pl>
2021-11-24 13:58:38 +0100

    Add icon to SearchResults

--- src/components/SearchResults/SearchResults.js
+++ src/components/SearchResults/SearchResults.js
@@ -2,21 +2,31 @@ import React from 'react';
 import styles from './SearchResults.scss';
 import PropTypes from 'prop-types';
 import Card from '../Card/Card.js';
+import Icon from '../Icon/Icon.js';
+import {settings} from '../../data/dataStore.js';
 
 class SearchResults extends React.Component {
 
   static propTypes = {
     cards: PropTypes.array,
- 
+    icon: PropTypes.node,
+  };
+
+  static defaultProps = {
+    icon: settings.search.icon,
   };
 
   render() {
 
-    const {cards} = this.props;
+    const {cards, icon} = this.props;
     
     return (
       <section className={styles.component}>
-
+        <h3 className={styles.title} >
+          <span className={styles.icon}>
+            <Icon name={icon}></Icon>
+          </span>
+        </h3>
         <div>
           {cards.map(cardData => (
             <Card key={cardData.id} {...cardData} />
