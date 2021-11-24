98643dc8462c8289e3e0b22c2c63218a97ec81be
Agnieszka Szostak <aa.szostak3@student.uw.edu.pl>
2021-11-24 13:49:50 +0100

    Change props in cardsRedux.js

--- src/redux/cardsRedux.js
+++ src/redux/cardsRedux.js
@@ -4,7 +4,7 @@ import shortid from 'shortid';
 export const getCardsForColumn = ({cards}, columnId ) => cards.filter(card => card.columnId == columnId 
   // && new RegExp(searchString, 'i').test(card.title),
 );
-export const filteredCardsForSearchResults = ({cards, searchString}) => cards.filter(card => new RegExp(searchString, 'i').test(card.title));
+export const filteredCardsForSearchResults = ({cards}, searchString) => cards.filter(card => new RegExp(searchString, 'i').test(card.title));
 
 // action name creator
 const reducerName = 'cards';
